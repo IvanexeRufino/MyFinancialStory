11d4cacd9a45e91782cb294ac06c1e298a2ce933
IvanRufino2471 <ivan.rufino@mercadolibre.com>
2019-04-28 13:54:35 -0300

    other fail

--- feature/src/androidTest/java/com/myfinancialstory/irufino/myfinancialstory/feature/ExampleInstrumentedTest.kt
+++ feature/src/androidTest/java/com/myfinancialstory/irufino/myfinancialstory/feature/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.myfinancialstory.irufino.myfinancialstory.feature.test", appContext.packageName)
+        assertEquals("com.myfinancialstory.irufino.myfinancialstory.feature", appContext.packageName)
     }
 }
